217481c70fa8ac1b37f09b9ee917a13a79f4f6e0
Matthew <Deweasematthew@gmail.com>
2023-10-03 16:27:15 -0500

    deleting carousel script

--- Assets/script.js
+++ Assets/script.js
@@ -1,20 +1,3 @@
-// https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&exclude={part}&appid={API key}
-
-// Parameters
-// lat	required	Latitude, decimal (-90; 90). If you need the geocoder to automatic convert city names and zip-codes to geo coordinates and the other way around, please use our Geocoding API
-// lon	required	Longitude, decimal (-180; 180). If you need the geocoder to automatic convert city names and zip-codes to geo coordinates and the other way around, please use our Geocoding API
-// appid	required	Your unique API key (you can always find it on your account page under the "API key" tab)
-// exclude	optional	By using this parameter you can exclude some parts of the weather data from the API response. It should be a comma-delimited list (without spaces).
-// Available values:
-
-// current
-// minutely
-// hourly
-// daily
-// alerts
-// units	optional	Units of measurement. standard, metric and imperial units are available. If you do not use the units parameter, standard units will be applied by default. Learn more
-// lang	optional	You can use the lang parameter to get the output in your language. Learn more
-
 var cityInput = document.getElementById("cityInput");
 var searchBtn = document.getElementById("searchBtn");
 var searchHistory = document.getElementById("searchHistory");
@@ -44,95 +27,74 @@ function getWeatherData(city) {
       API_KEY +
       "&units=metric";
 
-        fetch(apiURL)
-            .then(function(response) {
-                if (response.ok) {
-                    return response.json();
-                } else {
-                    alert('Error: ' + response.statusText);
-                }
-            })
-            .then(function(data) {
-                displayCurrentWeather(data);
-                return data.coord;
-            })
-            .then(function(coord) {
-                var forecastUrl =
-                  "https://api.openweathermap.org/data/2.5/onecall?lat=" + coord.lat + "&lon=" + coord.lon + "&exclude=current,minutely,hourly,alerts&appid=" + API_KEY + "&units=metric";
-                    fetch(forecastUrl)
-                        .then(function(response) {
-                            if (response.ok) {
-                                return response.json();
-                            } else {
-                                alert('Error: ' + response.statusText);
-                            }
-                        })
-                        .then(function(data) {
-                            displayForecast(data.daily);
-                        });
-            });
+         fetch(apiURL)
+        .then(response => response.json())
+        .then(data => {
+            displayCurrentWeather(data);
+            var lat = data.coord.lat;
+            var lon = data.coord.lon;
+            return fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + lat + "&lon=" + lon + "&appid=" + API_KEY + "&units=metric");
+        })
+        .then(response => response.json())
+        .then(data => {
+            displayForecast(data.list);
+        })
+        .catch(err => console.error("Error fetching data:", err));
 }
+
 function displayCurrentWeather(data) {
+    var tempFahrenheit = (data.main.temp * 9) / 5 + 32;
     currentCity.textContent = data.name + ", " + new Date().toLocaleDateString();
     weatherIcon.src = "https://openweathermap.org/img/wn/" + data.weather[0].icon + ".png";
-    currentTemperature.textContent = "Temperature: " + data.main.temp + "°C";
+    currentTemperature.textContent = "Temperature: " + tempFahrenheit.toFixed(1) + "°F";
     currentHumidity.textContent = "Humidity: " + data.main.humidity + "%";
     currentWindSpeed.textContent = "Wind Speed: " + data.wind.speed + " km/h";
 }
 
-function displayForecast(dailyData) {
-    while (forecastCarousel.firstChild) {
-        forecastCarousel.removeChild(forecastCarousel.firstChild);
-    }
-    for (var i = 0; i < 5; i++) {
-        var dailyData = dailyData[i];
-        
-         var carouselItem = document.createElement("div");
-         carouselItem.classList.add("carousel-item");
-         if (i === 0) {
-           carouselItem.classList.add("active");
-         }
+function displayForecast(forecastData) {
+   document.getElementById("forecastContainer").innerHTML = "";
 
-         var card = document.createElement("div");
-         card.classList.add("card");
+  for (var i = 0; i < 5; i++) {
+    var dayData = forecastData[i];
 
-         var cardBody = document.createElement("div");
-         cardBody.classList.add("card-body");
+    var card = document.createElement("div");
+    card.classList.add("card", "col-md-2", "ml-3");
+    
+    var card = document.createElement("div");
+    card.classList.add("card");
 
-         var cardTitle = document.createElement("h5");
-         cardTitle.classList.add("card-title");
-         cardTitle.textContent = new Date(
-           dailyData.dt * 1000
-         ).toLocaleDateString();
+    var cardBody = document.createElement("div");
+    cardBody.classList.add("card-body");
 
-         var weatherImg = document.createElement("img");
-         weatherImg.src =
-           "https://openweathermap.org/img/wn/" +
-           dailyData.weather[0].icon +
-           ".png";
+    var cardTitle = document.createElement("h5");
+    cardTitle.classList.add("card-title");
+    cardTitle.textContent = new Date(dayData.dt * 1000).toLocaleDateString();
 
-         var tempText = document.createElement("p");
-         tempText.classList.add("card-text");
-         tempText.textContent = "Temperature: " + dailyData.temp.day + "°C";
+    var weatherImg = document.createElement("img");
+    weatherImg.src =
+      "https://openweathermap.org/img/wn/" + dayData.weather[0].icon + ".png";
 
-         var windText = document.createElement("p");
-         windText.classList.add("card-text");
-         windText.textContent = "Wind Speed: " + dailyData.wind_speed + " km/h";
+    var tempText = document.createElement("p");
+    tempText.classList.add("card-text");
+    tempText.textContent = "Temperature: " + dayData.temp.day + "°C";
 
-         var humidityText = document.createElement("p");
-         humidityText.classList.add("card-text");
-         humidityText.textContent = "Humidity: " + dailyData.humidity + "%";
+    var windText = document.createElement("p");
+    windText.classList.add("card-text");
+    windText.textContent = "Wind Speed: " + dayData.wind_speed + " km/h";
 
-        cardBody.appendChild(cardTitle);
-        cardBody.appendChild(weatherImg);
-        cardBody.appendChild(windText);
-        cardBody.appendChild(tempText);
-        cardBody.appendChild(humidityText);
+    var humidityText = document.createElement("p");
+    humidityText.classList.add("card-text");
+    humidityText.textContent = "Humidity: " + dayData.humidity + "%";
 
-        card.appendChild(cardBody);
-        carouselItem.appendChild(card);
-        forecastCarousel.appendChild(carouselItem);
-    }
+    cardBody.appendChild(cardTitle);
+    cardBody.appendChild(weatherImg);
+    cardBody.appendChild(windText);
+    cardBody.appendChild(tempText);
+    cardBody.appendChild(humidityText);
+    card.appendChild(cardBody);
+    carouselItem.appendChild(card);
+    forecastCarousel.appendChild(carouselItem);
+  }
 }
 
 function addToSearchHistory(city) {
